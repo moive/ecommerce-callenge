e8e0d18564d3f2d7fa1f7242612d6b195dc40c38
Moises Velasquez <moises007@gmail.com>
2026-04-10 01:55:52 -0500

    refactor(card): changes card

--- src/app/shared/ui/product-card/product-card.component.ts
+++ src/app/shared/ui/product-card/product-card.component.ts
@@ -1,16 +1,38 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  output,
+  signal,
+} from '@angular/core';
 import { Product } from '../../../core/model';
 import { NgOptimizedImage } from '@angular/common';
+import { CartService } from '../../../core/services';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductCard {
-  product = input<Product>();
+  product = input.required<Product>();
 
-  add = output<Product>();
+  private cartService = inject(CartService);
+  private _isWishlisted = signal<boolean>(false);
+
+  isWishlisted = computed(() => this._isWishlisted());
+
+  addToCart(): void {
+    this.cartService.add(this.product());
+    this.cartService.openModal();
+  }
+
+  toggleWishlist(): void {
+    this._isWishlisted.update((v) => !v);
+  }
 }
